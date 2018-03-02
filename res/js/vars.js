62157ea50e25f41631cb37ce0eb27c5e1ef83b2d
ShakeelDXC <gkp.shakeel@gmail.com>
2018-03-02 11:54:57 +0800

    Update vars.js

--- res/js/vars.js
+++ res/js/vars.js
@@ -1,14 +1,17 @@
- var url_home = "https://rawgit.com/dxcuni/guidedlearn/itg";
- var url_image = "https://raw.githubusercontent.com/dxcuni/guidedlearn/itg/res/images/";
- var url_mode2 = "https://rawgit.com/dxcuni/guidedlearn/itg/pages/mode2page/modetwopage.html?page=";
+ var url_home = "https://rawgit.com/dxcuni/admintool/itg";
+ var url_image = "https://raw.githubusercontent.com/dxcuni/admintool/itg/res/images/";
+ var url_mode2 = "https://rawgit.com/dxcuni/admintool/itg/pages/mode2page/modetwopage.html?page=";
  var mode2Image = "10.jpg";
-    
+   //Home URL of courses
+var homeGuided       = "https://rawgit.com/dxcuni/guidedlearn/itg/";
+var homeTechLernReso = "https://rawgit.com/dxcuni/techlearnreso/itg/";
+var homeAddLernReso  = "https://rawgit.com/dxcuni/addtllearnreso/itg/";
 
 var app = angular.module('myApp', []);
 app.controller('myCtrl', function ($scope) {    
-    $scope.url_home = "https://rawgit.com/dxcuni/guidedlearn/itg";
-    $scope.url_image = "https://raw.githubusercontent.com/dxcuni/guidedlearn/itg/res/images/";
-    $scope.url_mode2 = "https://rawgit.com/dxcuni/guidedlearn/itg/mod2page.html?page=";
+    $scope.url_home = "https://rawgit.com/dxcuni/admintool/itg";
+    $scope.url_image = "https://raw.githubusercontent.com/dxcuni/admintool/itg/res/images/";
+    $scope.url_mode2 = "https://rawgit.com/dxcuni/admintool/itg/mod2page.html?page=";
     
     $scope.location = function (page) {
         var pages = JSON.parse(conf);
