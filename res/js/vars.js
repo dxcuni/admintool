5241b1e3ae7c634a4ef0fffc4d7e06ce281761dc
ShakeelDXC <gkp.shakeel@gmail.com>
2018-03-02 15:02:20 +0800

    Update vars.js

--- res/js/vars.js
+++ res/js/vars.js
@@ -3,10 +3,33 @@
  var url_mode2 = "https://rawgit.com/dxcuni/admintool/itg/pages/mode2page/modetwopage.html?page=";
  var mode2Image = "10.jpg";
    //Home URL of courses
-var homeGuided       = "https://rawgit.com/dxcuni/guidedlearn/itg/";
-var homeTechLernReso = "https://rawgit.com/dxcuni/techlearnreso/itg/";
-var homeAddLernReso  = "https://rawgit.com/dxcuni/addtllearnreso/itg/";
+var homeGuided       = "https://rawgit.com/dxcuni/guidedlearn/itg";
+var homeTechLernReso = "https://rawgit.com/dxcuni/techlearnreso/itg";
+var homeAddLernReso  = "https://rawgit.com/dxcuni/addtllearnreso/itg";
 var urlAdmintool     = "https://rawgit.com/dxcuni/admintool/itg//pages/addeditdata.html";
+
+
+var varConf = getParameterByName('page');
+var varCourse = getParameterByName('re');
+var varJsPage = "";
+varJsPage = getHomeURL(varCourse);
+alert(varJsPage);
+//get home url of the course start ///
+function getHomeURL(course) {
+    if (course === 'tech') {        
+        varJsPage = homeTechLernReso + '/res/json/'+varConf + '.js';
+    }
+    else if (course === 'additi') {        
+        varJsPage = homeAddLernReso + '/res/json/' + varConf + '.js';
+    }
+    else {        
+        varJsPage = homeGuided + '/res/json/' + varConf + '.js';
+    }
+    return varJsPage;
+}
+
+//get home url of the course END ///
+
 
 var app = angular.module('myApp', []);
 app.controller('myCtrl', function ($scope) {    
