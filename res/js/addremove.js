9a9d3a19a172e6b142813a0c23d219bd7980a7c8
ShakeelDXC <gkp.shakeel@gmail.com>
2018-03-02 15:41:58 +0800

    Update addremove.js

--- res/js/addremove.js
+++ res/js/addremove.js
@@ -386,7 +386,7 @@ function genMode2URL(urlObject) {
     var strRowIndex = urlObject.parentNode.parentNode.rowIndex;
     var strValue = $("#textBoxPage" + strRowIndex).val();
     if (jQuery.trim(strValue).length > 0) {
-        var url = '../tools/modepageaddedit.html?page=' + strValue;
+        var url = '../pages/modepageaddedit.html?page=' + strValue;
         window.open(url, '_blank');
     }
     else {
